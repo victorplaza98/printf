95e62f067aa676851a75be62fe17cc76e1ed9bfc
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-26 14:01:55 -0500

    Adavances debuging

--- betty_test/holberton.h
+++ betty_test/holberton.h
@@ -4,21 +4,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+
 /**
- * struct struct_format - Define a structure with a string format
+ * struct print_format - Define a structure with a string format
  * and function for print it.
  * @str_format: String of format.
  * @fun_print: Function for print.
  */
-typedef struct struct_format
+typedef struct print_format
 {
 	char *str_format;
 	int (*fun_print)(va_list);
-} struct_format;
+} print_format;
 
-
+/*Helper fuctions*/
 int _putchar(char c);
+
 int _printf(const char *format, ...);
-int check_funtion(const char *format, print_str list[], va_list arg_list);
+int print_c(va_list list);
+int print_s(va_list list);
 
 #endif
